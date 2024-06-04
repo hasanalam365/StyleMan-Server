66e2805b44702285f87b1d9974dfcbc35b58ebbf
hasanalam24 <hasanalam365@gmail.com>
2024-06-05 05:54:32 +0600

    create-request post api done

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(express.json())
+app.use(cors())
 
 
 
@@ -26,12 +28,79 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const usersCollection = client.db('BloodDonate').collection('users')
+        const districtsCollection = client.db('BloodDonate').collection('Districts')
+        const upazilasCollection = client.db('BloodDonate').collection('Upazilas')
+        const donationRequestCollection = client.db('BloodDonate').collection('donationRequest')
+
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/districts', async (req, res) => {
+            const result = await districtsCollection.find().toArray()
+            res.send(result)
+        })
+        app.get('/upazilas', async (req, res) => {
+            const result = await upazilasCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/user/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { email: email }
+            const result = await usersCollection.findOne(query)
+            res.send(result)
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user)
+            res.send(result)
+        })
+
+
+        app.patch('/users/:id', async (req, res) => {
+            const updatedinfo = req.body
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+
+            const updatedDoc = {
+                $set: {
+                    name: updatedinfo.name,
+                    email: updatedinfo.email,
+                    bloodGroup: updatedinfo.bloodGroup,
+                    district: updatedinfo.district,
+                    upazila: updatedinfo.upazila,
+                    photoURL: updatedinfo.photoURL,
+                }
+            }
+            console.log(req.body)
+            const result = await usersCollection.updateOne(filter, updatedDoc)
+            res.send(result)
+        })
+
+        // donation request related api
+        app.post('/create-donation-request', async (req, res) => {
+            const donationDetails = req.body;
+            const result = await donationRequestCollection.insertOne(donationDetails)
+            res.send(result)
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
