0a276c315f211addbb07b935157abcd931785c08
Hasan Alam <hasanalam365@gmail.com>
2025-08-18 23:24:25 +0600

    jwt done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
@@ -26,6 +27,25 @@ const client = new MongoClient(uri, {
   },
 });
 
+//middlewares
+const verifyToken = async (req, res, next) => {
+  // console.log('inside verify token', req.headers.authorization)
+
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = req.headers.authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -41,6 +61,15 @@ async function run() {
     const unPaidIncomeCollection = client
       .db("StyleMan")
       .collection("unPaidIncome");
+
+    //jwt related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body; //playload eta client site theke ashbe
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
 
     //monthly income page api
     app.get("/dailyIncome", async (req, res) => {
@@ -365,7 +394,7 @@ async function run() {
 
         const monthlyExpenseMap = {};
 
-        const currentYear = new Date().getFullYear(); // আজকের বছর (যেমন 2025)
+        const currentYear = new Date().getFullYear();
 
         getExpense.forEach((item) => {
           const dateStr = item.date;
